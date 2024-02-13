ceb126b6679cc713a8d4cf5dd1fc9731233774a8
Jamie Simms <jamiesimms@protonmail.com>
2024-02-13 15:31:04 +0000

    altered the view so that it shows only user data , or redirects to the login screen

--- login/views.py
+++ login/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from .models import Score
+from login.forms import ScoreForm
 
 # Create your views here.
 class ScoreList(generic.ListView):
     queryset = Score.objects.all()
     template_name='login/index.html'
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            scores = Score.objects.filter(user=request.user)
+            return render(request, 'login/index.html', {
+                'scores': scores
+            }
+            )
+        else:
+            return redirect('account_login')
