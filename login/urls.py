0f2caec206dc068002ce487589d753215fee9035
Jamie Simms <jamiesimms@protonmail.com>
2024-02-14 08:07:34 +0000

    added a view for handling POST requests for adding a new record

--- login/urls.py
+++ login/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path 
 
 urlpatterns = [
-    path('', views.ScoreList.as_view(),name='score')
+    path('', views.ScoreList.as_view(),name='score'),
+    path('add_score', views.add_score, name='add'),
 ]
--- login/views.py
+++ login/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views import generic
 from .models import Score
 from login.forms import ScoreForm
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 class ScoreList(generic.ListView):
@@ -20,3 +22,13 @@ class ScoreList(generic.ListView):
             )
         else:
             return redirect('account_login')
+        
+def add_score(request):
+    if request.method == 'POST':
+        score_form = ScoreForm(data=request.POST)
+
+        if score_form.is_valid():
+            score = score_form.save(commit=False)
+            score.user = request.user
+            score.save()
+    return HttpResponseRedirect(reverse('score'))
